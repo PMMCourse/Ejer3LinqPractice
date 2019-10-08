da83b799e3e88a642c181b7c110f24c04015197e
DaniRomeroR <daniel.romero.rodriguez2@gmail.com>
2019-10-08 12:59:50 +0200

    Añado los 3 primeros ejercicios.

--- Ejer3LinqPractice/Program.cs
+++ Ejer3LinqPractice/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Ejer3LinqPractice
 {
@@ -10,6 +12,38 @@ namespace Ejer3LinqPractice
     {
         static void Main(string[] args)
         {
+            String archivo = File.ReadAllText("Heroes.json");
+            List<Heroe> heroes= JsonConvert.DeserializeObject<List<Heroe>>(archivo);
+
+
+            /* foreach (var a in heroes)
+             {
+                 Console.WriteLine(a.Name);
+             }*/
+            Console.WriteLine(Ejer2(heroes));
+            Ejer3(heroes);
+            Console.ReadKey();
+        }
+        public static String Ejer2(List<Heroe> heroes) {
+            
+            var femenino=heroes.Count(x => x.Gender == "Female");
+            var masculino=heroes.Count(x => x.Gender == "Male");
+            String cadena = "Heroes masculinos:" + masculino+" Heroes femeninos:"+femenino;
+            return cadena;
+        }
+
+        public static void Ejer3(List<Heroe> heroes)
+        {
+            var ej3 = heroes.Where(x => x.Level >7);
+            Console.WriteLine("Los 10 primeros heroes con nivel superior a 7 son:");
+            int contador = 0;
+            foreach (var a in ej3)
+            {
+                if (contador<=10) {
+                    Console.WriteLine("Nombre: " + a.Name);
+                    contador++;
+                }
+            }
         }
     }
 }
---
+++ Ejer3LinqPractice/Properties/Ejer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Class1
+{
+	public Class1()
+	{
+        static void Main(string[] args)
+        {
+            String archivo = File.ReadAllText("Heroes.json");
+            List<Heroe> heroes = JsonConvert.DeserializeObject<List<Heroe>>(archivo);
+        }
+    }
+}
