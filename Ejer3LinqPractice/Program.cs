5711428fb493fa998608c2b5474b27ad4626febe
DaniRomeroR <daniel.romero.rodriguez2@gmail.com>
2019-10-08 19:55:35 +0200

    Añadido ejercicio 6,7 y 8

--- Ejer3LinqPractice/Program.cs
+++ Ejer3LinqPractice/Program.cs
@@ -15,10 +15,13 @@ namespace Ejer3LinqPractice
             String archivo = File.ReadAllText("Heroes.json");
             List<Heroe> heroes= JsonConvert.DeserializeObject<List<Heroe>>(archivo);
 
-            Console.WriteLine(Ejer2(heroes));
+            /*Console.WriteLine(Ejer2(heroes));
             Ejer3(heroes);
             Ejer4(heroes);
             Ejer5(heroes);
+            Ejer6(heroes);
+            Ejer7(heroes);*/
+            Ejer8(heroes);
             Console.ReadKey();
         }
         public static String Ejer2(List<Heroe> heroes) {
@@ -37,7 +40,7 @@ namespace Ejer3LinqPractice
             foreach (var a in ej3)
             {
                 if (contador<=10) {
-                    Console.WriteLine("Nombre: " + a.Name);
+                    Console.WriteLine("Nombre: " + a.Name + " Hp: " + a.Hp + " MP: " + a.Mp + " Clase: " + a.Class + " Nivel: " + a.Level + " Género: " + a.Gender + " Descripcion: " + a.Description);
                     contador++;
                 }
             }
@@ -48,7 +51,7 @@ namespace Ejer3LinqPractice
             var ej4 = heroes.OrderBy(x => x.Class);
             foreach (var a in ej4)
             {
-                    Console.WriteLine("Nombre: " + a.Name +" Clase: "+a.Class);
+                Console.WriteLine("Nombre: " + a.Name + " Hp: " + a.Hp + " MP: " + a.Mp + " Clase: " + a.Class + " Nivel: " + a.Level + " Género: " + a.Gender + " Descripcion: " + a.Description);
             }
         }
 
@@ -67,5 +70,36 @@ namespace Ejer3LinqPractice
             }
         }
 
+        public static void Ejer6(List<Heroe> heroes)
+        {
+            var ej6 = heroes.Where(x => x.Hp > 50 & x.Gender=="Female");
+            int contador=0;
+            foreach (var a in ej6)
+            {
+                if (contador <= 10)
+                {
+                    Console.WriteLine("Nombre: " + a.Name);
+                    contador++;
+                }
+            }
+        }
+
+        public static void Ejer7(List<Heroe> heroes)
+        {
+            var ej7 = heroes.Where(x => x.Description==null);
+            foreach (var a in ej7)
+            {
+                    Console.WriteLine("Nombre: " + a.Name+" Hp: "+a.Hp+" MP: "+a.Mp+" Clase: "+a.Class+" Nivel: "+a.Level+" Género: "+a.Gender+" Descripcion: "+a.Description);
+            }
+        }
+
+        public static void Ejer8(List<Heroe> heroes)
+        {
+            var ej8 = heroes.Where(x => x.Gender=="Male" & x.Hp > 100 & x.Level > 10);
+            foreach (var a in ej8)
+            {
+                Console.WriteLine("Nombre: " + a.Name + " Hp: " + a.Hp + " MP: " + a.Mp + " Clase: " + a.Class + " Nivel: " + a.Level + " Género: " + a.Gender + " Descripcion: " + a.Description);
+            }
+        }
     }
 }
