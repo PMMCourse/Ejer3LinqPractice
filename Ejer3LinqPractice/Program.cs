d2eb34f4964f7700c3680fa4c7564a495bd14333
Youshiro <daniel.albarracin.m@gmail.com>
2019-10-09 20:10:31 +0200

    Subo hasta ejercicio 12

--- Ejer3LinqPractice/Program.cs
+++ Ejer3LinqPractice/Program.cs
@@ -37,26 +37,114 @@ namespace Ejer3LinqPractice
         //Ejercicio 3: Diez primeros personajes con nivel mayor que 7:
         public static void Ejer3(List<Heroe> h)
         {
-            var nombres = h.Where(nivel => nivel.Level > 7);
-            List<Heroe> lista = nombres.ToList();
+            var nombres = h.Where(nivel => nivel.Level > 7).ToList();
 
             for ( int i=0; i<=10; i++)
             {
-                Console.WriteLine(lista[i].Name);
+                Console.WriteLine(nombres[i].Name);
             }
         }
 
         //Ejercicio 4: Agrupar personajes por clase:
-        //PD: Intente hacerlo con GroupBy pero era un codigo mas largo para el mismo resultado.
         public static void Ejer4(List<Heroe> h)
         {
-            var clases = h.OrderBy(clase => clase.Class);
+            var grupo = h.GroupBy(clase => clase.Class);
 
-            foreach(var x in clases)
+            foreach(var x in grupo)
             {
-                Console.WriteLine($"Nombre de Heroe: {x.Name} Clase: {x.Class}");
-            }
+                Console.WriteLine(string.Format("Clase: {0}", x.Key));
 
+                foreach(var y in x)
+                {
+                    Console.WriteLine($"Nombre: {y.Name}");
+                }
+            }
+        }
+
+        //Ejercicio 5: 10 primero nombres de personajes con mas de 50 HP.
+        public static void Ejer5(List<Heroe> h)
+        {
+            var nombres = h.Where(hp => hp.Hp > 50).ToList();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(nombres[i].Name);
+            }
+        }
+
+        //Ejercicio 6: 10 primeros nombres de personajes femeninos con mas de 50 HP.
+        public static void Ejer6(List<Heroe> h)
+        {
+            var nombres = h.Where(x => x.Gender == "Female" && x.Hp > 50).ToList();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(nombres[i].Name);
+            }
+        }
+
+        //Ejercicio 7: Personajes con descripcion vacia:
+        public static void Ejer7(List<Heroe> h)
+        {
+            var nombres = h.Where(x => x.Description == null).ToList();
+
+            for (int i = 0; i < nombres.Count; i++)
+            {
+                Console.WriteLine(nombres[i].Name);
+            }
+        }
+
+        //Ejercicio 8: Primer Heroe masculino, mas de 100 de HP y nivel 10.
+        public static void Ejer8(List<Heroe> h)
+        {
+            var nombre = h.Where(x => x.Gender == "Male" && x.Hp > 100 && x.Level == 10).ToList();
+
+            if(nombre.Min() != null)
+            {
+                Console.WriteLine(nombre[0].Name);
+            }
+            else
+            {
+                Console.WriteLine("No existe ningun heroe");
+            }     
+        }
+
+        //Ejercico 9: Ordenar de la A a la Z.
+        public static void Ejer9(List<Heroe> h)
+        {
+            var orden = h.OrderBy(x => x.Name);
+
+            foreach (var x in orden)
+            {
+                Console.WriteLine($"Nombre de Heroe: {x.Name}");
+            }
+        }
+
+        //Ejercicio 10: Ordenar de la Z a la A.
+        public static void Ejer10(List<Heroe> h)
+        {
+            var orden = h.OrderByDescending(x => x.Name);
+
+            foreach (var x in orden)
+            {
+                Console.WriteLine($"Nombre de Heroe: {x.Name}");
+            }
+        }
+
+        //Ejercicio 11: Sumar HP de todos los Guerreros.
+        public static void Ejer11(List<Heroe> h)
+        {
+            var hpTotal = h.Where(x => x.Class == "Warrior").Sum(y => y.Hp);
+
+            Console.WriteLine($"Hp total: {hpTotal}");
+        }
+
+        //Ejercio 12: Sumar MP de todos los bardos.
+        public static void Ejer12(List<Heroe> h)
+        {
+            var mpTotal = h.Where(x => x.Class == "Bard" && x.Level > 3).Sum(y => y.Mp);
+
+            Console.WriteLine($"Mp total: {mpTotal}");
         }
     }
 }
